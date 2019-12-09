18d75e9ba76c4e64c96446f2a9a9948b44d865ba
Hoss Shah <hossein.shbz@gmail.com>
2019-12-09 02:48:44 -0800

    clean up

--- Assets/Scripts/HighlightManager.cs
+++ Assets/Scripts/HighlightManager.cs
@@ -86,7 +86,7 @@ public class HighlightManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (!_target.enabled) {
+        if (_target != null && !_target.enabled) {
             SetHighlightTarget(_target, false);
             _target = null;
             capsuleCollider.radius = 3f;
